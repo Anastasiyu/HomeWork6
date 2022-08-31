afdf076de6b56487c553467f6458119ead901dbd
Anastasiyu <asy.safiulina@mail.ru>
2022-08-31 23:06:36 +0300

    ДЗ задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { task1(); }
 
+
+           int[]arr=generateRandomArray();
+         public static int[]generateRandomArray(){
+        java.util.Random randome=new java.util.Random();
+        int[]arr=new int[30];
+        for(int i=0;i<arr.length;i++){
+
+        arr[i]=randome.nextInt (100_000)+100_000;
+        }
+        return arr;
+        }
+
+
+    public static void task1() {
+     int[] array = generateRandomArray();
+     int sum = 0;
+     for (int spending : array) {
+         sum += spending;
+     }
+        System.out.printf("Сумма трат за месяц составила %s рублей", sum);
     }
-  public static void task1 () {
-
-  }
-  }
+        }
